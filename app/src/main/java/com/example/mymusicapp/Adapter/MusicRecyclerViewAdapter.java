3b09e1052a5e1c8c7871ec5e2ab82a7b8cb96b24
Miftah Amirul Amin <miftah.amin@vostra.co.id>
2023-03-09 17:05:36 +0700

    Update Recycler View in Main Activity

--- app/src/main/java/com/example/mymusicapp/Adapter/MusicRecyclerViewAdapter.java
+++ app/src/main/java/com/example/mymusicapp/Adapter/MusicRecyclerViewAdapter.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.mymusicapp.R;
 import com.example.mymusicapp.API.Song;
 
@@ -36,8 +39,13 @@ public class MusicRecyclerViewAdapter extends RecyclerView.Adapter<MusicRecycler
         final Song song = songList.get(position);
         String title = song.getTitle();
         String singer = song.getSinger();
+        String imageUrl = "https://disk.mediaindonesia.com/files/news/2022/12/30/WhatsApp%20Image%202022-12-22%20at%2017.07.10%20(1).jpg";
         holder.musicTitle.setText(title);
         holder.musicSinger.setText(singer);
+        Glide.with(holder.itemView.getContext())
+                .load(imageUrl)
+                .apply(new RequestOptions().fitCenter())
+                .into(holder.albumImage);
     }
 
     @Override
@@ -47,10 +55,12 @@ public class MusicRecyclerViewAdapter extends RecyclerView.Adapter<MusicRecycler
 
     public class MusicRecyclerViewViewHolder extends RecyclerView.ViewHolder{
         TextView musicTitle, musicSinger;
+        ImageView albumImage;
         public MusicRecyclerViewViewHolder(@NonNull View itemView) {
             super(itemView);
             musicTitle = itemView.findViewById(R.id.text_title);
             musicSinger = itemView.findViewById(R.id.text_singer);
+            albumImage = itemView.findViewById(R.id.album_image);
         }
     }
 }
--- app/src/main/java/com/example/mymusicapp/Interface/JsonPlaceHolderApi.java
+++ app/src/main/java/com/example/mymusicapp/Interface/JsonPlaceHolderApi.java
@@ -8,7 +8,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface JsonPlaceHolderApi {
-
     @GET("song")
     Call<List<Song>> getSongs();
 }
