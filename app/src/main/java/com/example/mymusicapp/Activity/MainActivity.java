e6dd1e2bfc11e9bcf78c534422c3590cf1c89a61
Miftah Amirul Amin <miftah.amin@vostra.co.id>
2023-03-09 15:49:54 +0700

    Update Recycler View in Main Activity

---
+++ app/src/main/java/com/example/mymusicapp/Activity/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.mymusicapp.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.mymusicapp.Adapter.MusicRecyclerViewAdapter;
+import com.example.mymusicapp.R;
+import com.example.mymusicapp.Retrofit.Get;
+import com.example.mymusicapp.API.Song;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerViewMusic;
+    MusicRecyclerViewAdapter adapter;
+    LinearLayoutManager linearLayoutManager;
+    Get getRetrofit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerViewMusic = findViewById(R.id.recycler_view);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerViewMusic.setLayoutManager(linearLayoutManager);
+
+        getRetrofit = new Get();
+        Call<List<Song>> call = getRetrofit.getMusic();
+        call.enqueue(new Callback<List<Song>>() {
+            @Override
+            public void onResponse(Call<List<Song>> call, Response<List<Song>> response) {
+                if(response.isSuccessful()){
+                    List<Song> songs = response.body();
+                    adapter = new MusicRecyclerViewAdapter(MainActivity.this,songs);
+                    recyclerViewMusic.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Song>> call, Throwable t) {
+
+            }
+        });
+    }
+}
