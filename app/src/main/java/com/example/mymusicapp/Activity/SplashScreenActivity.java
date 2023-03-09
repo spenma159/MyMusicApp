94ac2f5be9f9f0c72705688da2cc44a341cbfcad
Miftah Amirul Amin <miftah.amin@vostra.co.id>
2023-03-09 17:23:53 +0700

    Update Recycler View in Main Activity

---
+++ app/src/main/java/com/example/mymusicapp/Activity/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.example.mymusicapp.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mymusicapp.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        View view = getWindow().getDecorView();
+        view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        if(getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
